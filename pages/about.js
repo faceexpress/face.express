c7976f8bb16d214deed20684fdef602eea8751a2
Sergey Lukin <contact@sergeylukin.com>
2018-06-11 01:42:43 +0300

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
+
+export default () => (
+  <div>
+    <Nav />
+
+    <div className="hero">{ "About" }</div>
+  </div>
+)
